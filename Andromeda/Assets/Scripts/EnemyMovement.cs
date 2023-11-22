65eff7e619889839daf36d45e562fd930d1e826e
CoolGuyGames <theforestgriefer@gmail.com>
2023-11-22 10:28:23 -0500

    panet and such

---
+++ Andromeda/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private bool grounded;
+    private GameObject player;
+
+    private Rigidbody rb;
+    private bool isJumping = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    private void Update()
+    {
+        if(!isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(Jump());
+        }
+    }
+    IEnumerator Jump()
+    {
+        yield return new WaitForSeconds(2);
+        rb.AddForce((player.transform.position - transform.position).normalized * 500);
+        isJumping = false;
+    }
+
+    private void FixedUpdate()
+    {
+        grounded = false;
+    }
+
+    void OnCollisionStay()
+    {
+        grounded = true;
+    }
+}
